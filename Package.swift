affb49719e0db2adff554b41dbb96996a97f1b66
bayoli-j <bayoli.j@infoset.cd>
2025-11-10 15:22:28 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,20 @@
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
-   name: "flexPaySharedKit",
+   name: "FlexPaySharedKit",
    platforms: [
      .iOS(.v14),
    ],
    products: [
-      .library(name: "flexPaySharedKit", targets: ["flexPaySharedKit"])
+      .library(name: "FlexPaySharedKit", targets: ["FlexPaySharedKit"])
    ],
    targets: [
       .binaryTarget(
-         name: "flexPaySharedKit",
-         url: "https://github.com/bayoli-j/Flexpaie/releases/download/Release/flexPaySharedKit.zip",
-         checksum:"786219ce6a93feced2d66a24956173e29c5a10d35ae0f95af4cce39ac6df0148")
+         name: "FlexPaySharedKit",
+         url: "https://github.com/bayoli-j/FlexPay-Multiplaforme-SDK/releases/download/Release/FlexPaySharedKit.zip",
+         checksum:"255063797a83dac72c0b765a2141424354786e7f19b91b24341d1a6e38f6d985"
+         )
    ]
 )
